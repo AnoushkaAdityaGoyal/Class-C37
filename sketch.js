5d40f5c4db70cf3f98a190d021dddbecb2c9bc31
AnoushkaAdityaGoyal <74051619+AnoushkaAdityaGoyal@users.noreply.github.com>
2021-01-29 17:04:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var canvas,backgroundImage,database;
+var playerCount;
+var gameState=0;
+var form,player,game;
+var allPlayers;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  canvas = createCanvas(400,400);
+  game=new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+   if(playerCount=4){
+     game.update(1);
+   }
+   if(gameState===1){
+     clear();
+     game.play();
+   }
+}
+
